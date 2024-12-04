d9d44300c1de72a130ba5d6e9115558e2925bbe7
kambains226 <121152303+kambains226@users.noreply.github.com>
2024-12-04 12:46:19 +0000

    working making it create  members txt file if it does not exist

--- src/main/java/org/openjfx/view/LoginPage.java
+++ src/main/java/org/openjfx/view/LoginPage.java
@@ -13,15 +13,13 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import java.awt.*;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+
+import java.util.Arrays;
 //extends vbox  to allign elements vertically
 public class LoginPage extends VBox{
 
-    private String memberPath = "/org/openjfx/data/members.txt";
+    public String memberPath = "/org/openjfx/data/members.txt";
     public LoginPage() {
         // create the user name and password
         Label labe = new Label("Login");
@@ -41,18 +39,66 @@ public class LoginPage extends VBox{
     }
     //checks if the user entered valid information
     private void loginCheck(String username, String password) {
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResource(memberPath)))) {
+        File file = new File(getClass().getResourceAsStream(memberPath));
+        File parent = file.getParentFile();
+
+
+
+
+//        if(!file.exists()){
+//            System.out.println("File does not exist");
+//            CreateFile(file);
+//        }
+//        else{
+//            System.out.println("File already exists");
+//        }
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(memberPath)))) {
             // gets the member file in the resource folder
+
+
             String line;
             while((line = reader.readLine()) !=null){
-                System.out.println(line);
+                String[] lineSplit =line.split(" ");
+                if(lineSplit[0].equals(username)  && lineSplit[1].equals(password)){
+                   System.out.println("signed in");
+                   login();
+                }
+                else{
+                    System.out.println("not signed in");
+                }
+            }
+        }
+
+        catch (IOException e){
+
+            System.out.println("d");
+            System.out.println(e); // prints the error
+        }
+        catch (Exception e)
+        {
+            CreateFile(file);
+            e.printStackTrace();
+        }
+
+    }
+    //sends the user to the home page of the website
+    private void login() {
+
+    }
+    private void CreateFile(File newFile) {
+        System.out.println(newFile.getPath());
+
+        try{
+            if(newFile.createNewFile()){
+                System.out.println("file created"+newFile.getName());
+            }
+            else{
+                System.out.println("file not created");
             }
         }
         catch (IOException e){
-            System.out.println(e); // prints the error
+            e.printStackTrace();
         }
-       catch(Exception e){
-            System.out.println(e);
-       }
+        System.out.println("created");
     }
 }
