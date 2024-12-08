d74401550fc0205ffc24b32884ba280b85afd116
kambains226 <121152303+kambains226@users.noreply.github.com>
2024-12-08 18:57:52 +0000

    got the fxml main scene to work

--- src/main/java/org/openjfx/controllers/MainController.java
+++ src/main/java/org/openjfx/controllers/MainController.java
@@ -1,4 +1,26 @@
-package org.openjfx;
+package org.openjfx.controllers;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 public class MainController {
+    @FXML
+    private LineChart <String ,Number> LineChart; //gets the line chart
+    @FXML
+    private CategoryAxis months ;
+
+    @FXML
+    private NumberAxis yAxis;
+
+
+    public void initialize(){
+         String[] monthsLabels = {"JAN","FEB","MAR","APR","MAY","JUNE","JULY","AUG","SEP","OCT","NOV","DEC"};
+
+
+        months.setCategories(javafx.collections.FXCollections.observableArrayList(monthsLabels));
+    }
+
 }
